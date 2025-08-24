37008032cc1c67c787f56a4a6c67aafb2ecd8c21
NASSAM Pêlê Deb <deb12nassam@gmail.com>
2025-08-24 22:50:00 +0200

    Initial commit : Application Magazine Info Flutter

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MonAppli());
+}
+
+class MonAppli extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Magazine',
+      debugShowCheckedModeBanner: false,
+      home: PageAccueil(),
+    );
+  }
+}
+
+class PageAccueil extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Magazine Infos"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Image.asset("assets/images/magazineInfo.jpg"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Tu as cliqué dessus')),
+          );
+        },
+        child: const Text("Click"),
+      ),
+    );
+  }
+}
